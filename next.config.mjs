1ac75af444eaad2f50570896016e2a783d660c9a
Phong Chu <chucamphong1999@gmail.com>
2026-08-08 21:09:05 +0700

    Init project

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
+
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isUserOrOrgPage = repoName?.endsWith('.github.io');
+const basePath = isGithubPages && repoName && !isUserOrOrgPage ? `/${repoName}` : '';
+
+/** @type {import('next').NextConfig} */
+const config = {
+  output: 'export',
+  reactStrictMode: true,
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
+  serverExternalPackages: ['@takumi-rs/core'],
+};
+
+export default withMDX(config);
